afeafcab774f71a84f907ac45503a8794da98ec7
Emmanuel Israel <emmaisraeln@gmail.com>
2026-07-21 10:10:11 +0100

    Add classifiers to models.py

---
+++ src/models.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    ExtraTreesClassifier
+)
+
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.svm import SVC
+
+from xgboost import XGBClassifier
