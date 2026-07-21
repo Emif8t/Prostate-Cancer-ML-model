65576c88005201b6a98363b43627943c60af8688
Emmanuel Israel <emmaisraeln@gmail.com>
2026-07-21 10:55:05 +0100

    Add clinical and molecular features to config.py

--- config.py
+++ config.py
@@ -1,3 +1,34 @@
+CLINICAL_FEATURES = [
+
+    "PSA",
+
+    "age"
+
+]
+
+MOLECULAR_FEATURES = [
+
+    "Expression_ASS1",
+
+    "Expression_CPS1",
+
+    "Methylation_CPS1",
+
+    "Methylation_CPS1",
+
+    "SNP_1", 
+    
+    "SNP_2", 
+  
+    "SNP_3"
+
+]
+
+
+COMBINED_FEATURES = CLINICAL_FEATURES + MOLECULAR_FEATURES
+
+TARGET_COLUMN = "Group"
+
 RANDOM_STATE = 42
 
 OUTER_FOLDS = 10
@@ -5,5 +36,3 @@ OUTER_FOLDS = 10
 INNER_FOLDS = 5
 
 BOOTSTRAP_ITERATIONS = 2000
-
-THRESHOLD = 0.5
