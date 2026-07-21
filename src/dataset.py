e8ae6fd0c4d19c7be73389ba110aae022212a0c4
Emmanuel Israel <emmaisraeln@gmail.com>
2026-07-21 10:42:19 +0100

    Implement Dataset class for loading Excel data
    Add Dataset class to load data from an Excel file.

--- src/dataset.py
+++ src/dataset.py
@@ -1,1 +1,18 @@
 import pandas as pd
+
+
+class Dataset:
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.df = None
+
+    def load(self):
+        """
+        data = Dataset("data/ROCdata2.xlsx")
+
+        df = data.load()
+        """
+        self.df = pd.read_excel(self.filepath)
+
+        return self.df
