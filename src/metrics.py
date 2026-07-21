d73b9e2c8222a26395d0c70b3de2e6684b09e732
Emmanuel Israel <emmaisraeln@gmail.com>
2026-07-21 10:13:11 +0100

    Add metrics.py with sklearn metrics imports

---
+++ src/metrics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from sklearn.metrics import (
+    roc_auc_score,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    brier_score_loss,
+    confusion_matrix
+)
