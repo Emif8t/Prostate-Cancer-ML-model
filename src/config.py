b5f3aff04dc2b18229f5e787cf347568734c452f
Emmanuel Israel <emmaisraeln@gmail.com>
2026-07-22 13:00:20 +0100

    Add configuration settings for data analysis

---
+++ src/config.py
@@ -0,0 +1,42 @@
+DATA_PATH = "C:/Users/USER/Desktop/data_analysis/ROCdata2.xlsx"
+
+CLINICAL_FEATURES = [
+
+    "PSA",
+
+    "age"
+
+]
+
+MOLECULAR_FEATURES = [
+
+    "Expression_ASS1",
+
+    "Expression_CPS1",
+
+    "Methylation_ASS1",
+
+    "Methylation_CPS1",
+
+    "SNP_1", 
+    
+    "SNP_2", 
+  
+    "SNP_3"
+
+]
+
+
+COMBINED_FEATURES = CLINICAL_FEATURES + MOLECULAR_FEATURES
+
+TARGET_COLUMN = "Group"
+
+RANDOM_STATE = 42
+
+OUTER_FOLDS = 10
+
+INNER_FOLDS = 5
+
+BOOTSTRAP_ITERATIONS = 2000
+
+PROBABILITY_THRESHOLD = 0.5
